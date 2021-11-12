6c137c2ff2e379d8b74e0f27be4dd75c0f0f4dd8
alexandra-lezcano <alexandra.lezcano@fiuni.edu.py>
2021-11-11 22:43:56 -0300

    fix algunas cosas

--- src/RestClientTest.java
+++ src/RestClientTest.java
@@ -5,19 +5,28 @@ import User.UserManager;
 public class RestClientTest {
     public static void main(String[] args) {
         UserManager userManager = new UserManager();
-       // userManager.addUser();
-        /*userManager.getUser();
-        userManager.updateUser();*/
 
-       CityManager cityManager = new CityManager();
-      // cityManager.addCity("Encarnacion", "Test 1");
-        cityManager.getById(2);
-      //  cityManager.updateCity();
 
-      NeighborhoodManager neighborhoodManager = new NeighborhoodManager();
-      // neighborhoodManager.addNeighborhood("Test abrrio asuncion","test description..", 2);
-      //  neighborhoodManager.addNeighborhood("San Pedro","test description..", 1);
-    //    neighborhoodManager.updateNeighborhood(2);
 
+      CityManager cityManager = new CityManager();
+
+
+
+    NeighborhoodManager neighborhoodManager = new NeighborhoodManager();
+
+        cityManager.addCity("Encarnacion", "Test 1");
+        cityManager.addCity("Ciudad2", "Test 2");
+        // cityManager.getById(2);
+        // cityManager.updateCity();
+
+        neighborhoodManager.addNeighborhood("Barrio1","test description..", 1);
+        neighborhoodManager.addNeighborhood("Barrio2","test description..", 1);
+        //  neighborhoodManager.addNeighborhood("San Pedro","test description..", 1);
+        //  neighborhoodManager.updateNeighborhood(2);
+
+        userManager.addUser();
+         //userManager.getUser();
+        // userManager.updateUser();
+        //userManager.deleteUser(1);
     }
 }
--- src/User/UserManager.java
+++ src/User/UserManager.java
@@ -2,12 +2,18 @@ package User;
 
 import Base.AbstractBaseManager;
 import com.protectionapp.sd2021.dto.user.UserDTO;
+import com.sun.javafx.image.IntPixelGetter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class UserManager extends AbstractBaseManager {
 
     public UserManager(){
         super();
     }
+
+    public String pathToResource = "/users";
 
     public void addUser(){
         UserDTO userTest = new UserDTO();
@@ -18,11 +24,19 @@ public class UserManager extends AbstractBaseManager {
         userTest.setCn(3996354); // could be string also
         userTest.setAddress("742 Evergreen Terrace");
 
-        getJerseyClient().resource(getBaseUrl()+"/users").entity(userTest).post(UserDTO.class);
+        Set<Integer> neighborhoodIds = new HashSet<>();
+        neighborhoodIds.add(1);
+        neighborhoodIds.add(2);
+
+        userTest.setCityId(1);
+        userTest.setNeighborhoods(neighborhoodIds);
+        userTest.setRole_id(2);
+        UserDTO newUser = getJerseyClient().resource(getBaseUrl()+pathToResource).entity(userTest).post(UserDTO.class);
+        System.out.println("TEST POST /users " +newUser.toString());
     }
 
     public void getUser(){
-        getJerseyClient().resource(getBaseUrl()+"/users/2").get(UserDTO.class);
+        getJerseyClient().resource(getBaseUrl()+pathToResource+"/2").get(UserDTO.class);
     }
     public void updateUser(){
         UserDTO userTest = new UserDTO();
@@ -33,12 +47,18 @@ public class UserManager extends AbstractBaseManager {
         userTest.setCn(3996354); // could be string also
         userTest.setAddress("742 Evergreen Terrace");
 
-        getJerseyClient().resource(getBaseUrl()+"/users").entity(userTest).post(UserDTO.class);
+        getJerseyClient().resource(getBaseUrl()+pathToResource).entity(userTest).post(UserDTO.class);
 
         userTest.setName("asdf");
         userTest.setUsername("asdf");
 
-        getJerseyClient().resource(getBaseUrl()+"/users/2").entity(userTest).put(UserDTO.class);
+        getJerseyClient().resource(getBaseUrl()+pathToResource+"/2").entity(userTest).put(UserDTO.class);
 
+    }
+
+    public void deleteUser(Integer id){
+        UserDTO deletedUser =  getJerseyClient().resource(getBaseUrl()+pathToResource+"/"+id).delete(UserDTO.class);
+
+        System.out.println("DELETE TEST "+deletedUser.toString());
     }
 }
